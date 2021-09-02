e7bd9a01aedb5fb3b1375fec5fc7cd9fb989173c
jenny uppal <42324520+AvicJennyRjn@users.noreply.github.com>
2021-09-02 15:50:16 +0700

    adding files

--- app.js
+++ app.js
@@ -1,38 +1,62 @@
-var ul_list = document.querySelector('.list-ul');
-var todo_button = document.querySelector('.todo-button');
+var ul_list = document.querySelector('.list_ul');
+var todo_button = document.querySelector('.todo_button');
+var todo_input = document.querySelector('.todo_input');
 
 
+
+// event listeners
 todo_button.addEventListener("click",addtodo);
+ul_list.addEventListener("click", delete_list);
 
 
 function addtodo(event){
     event.preventDefault(); //prevents from default behavior of event i think
     
     var list_div = document.createElement('div');
-    list_div.classList.add('.todo_div');
+    list_div.classList.add('todo_div');
     var li_element = document.createElement('li');
 
     // appending
     ul_list.appendChild(list_div); //appending to the ul
+    
+
+    //adding list
+    li_element.classList.add('list_content');
     list_div.appendChild(li_element);
 
+    li_element.innerText = todo_input.value;
+
     //delete and completed creating elements
-    var delete_btn = document.createElement('button');
+    var deleted_btn = document.createElement('button');
     deleted_btn.innerHTML = '<i class="fas fa-trash-alt"></i>';
-    var deleted = deleted_btn.classList.add('.delete_button');
+    deleted_btn.classList.add('delete_button');
 
     var completed_btn = document.createElement('button');
     completed_btn.innerHTML = '<i class="far fa-check-square"></i>';
-    var complete = completed_btn.classList.add('.complete');
+    completed_btn.classList.add('complete');
 
     //adding icons to delete and completed
    
 
     //appending delete and completed elements to list_div
-    list_div.appendChild(deleted);
-    list_div.appendChild(complete);
+    
+    list_div.appendChild(deleted_btn);
+    list_div.appendChild(completed_btn);
 
 
-
+    //clear input value
+    todo_input.value = "";
 
 }
+
+function delete_list(e){
+    var target = e.target;
+    if(target.classList[0]==='delete_button'){
+        target.parentElement.remove();
+    }
+    if(target.classList[0]==='complete'){
+        var parent_div = target.parentElement;
+        parent_div.classList.toggle('completed_deco');
+    }
+}
+
